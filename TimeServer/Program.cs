6613c9f4a299aac712e79b6bd889481e0e6b0937
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2024-09-24 16:12:16 +0200

    Caan print time and probably connect multiple times

--- TimeServer/Program.cs
+++ TimeServer/Program.cs
@@ -1,17 +1,24 @@
-﻿using System.Net;
+﻿using System.Collections;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
-Console.WriteLine("Starting Up the server...");
+Console.WriteLine("Starting up server...");
 var listener = new TcpListener(IPAddress.Any, 44444);
 listener.Start();
-Console.WriteLine("Listening for incoming connections on" + listener.LocalEndpoint + "...");
-var client = listener.AcceptTcpClient();
-Console.WriteLine("Server accepted: " + client.Client.RemoteEndPoint);
-string currentTime = "Tuesday, today!";
 
-byte[] bytes = Encoding.ASCII.GetBytes(currentTime);
-
-client.GetStream().Write(bytes);
-client.Close();
-
+while (true)
+{
+    Console.WriteLine("Listening for incoming connections on " + listener.LocalEndpoint + "...");
+   
+    var client = listener.AcceptTcpClient(); // blocking
+    Console.WriteLine("Client accepted: " + client.Client.RemoteEndPoint);
+   
+    string currentTime = "This is Marina's server, current time is: " + DateTime.Now;
+   
+    byte[] bytes = Encoding.ASCII.GetBytes(currentTime);
+  
+    client.GetStream().Write(bytes);
+    client.Close();
+    Console.WriteLine("Closed connection...");
+}
