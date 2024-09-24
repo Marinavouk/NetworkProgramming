780b215050540a32d9fe4caf2ad2b536d85780ae
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2024-09-24 15:09:20 +0200

    created a client that connects to my self

---
+++ TimeClient/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Console.WriteLine("Connecting to the server...");
+var client = new TcpClient();
+client.Connect("127.0.0.1", 44444);
+Console.WriteLine("Server accepted" + client.Client.RemoteEndPoint);
+
+var buffer = new byte[1000];
+var bytesRecieved = client.GetStream().Read(buffer,0,buffer.Length);
+
+var currentTime = Encoding.ASCII.GetString(buffer);
+Console.WriteLine(currentTime);
--- TimeServer/Program.cs
+++ TimeServer/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Marina");
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Console.WriteLine("Starting Up the server...");
+var listener = new TcpListener(IPAddress.Any, 44444);
+listener.Start();
+Console.WriteLine("Listening for incoming connections on" + listener.LocalEndpoint + "...");
+var client = listener.AcceptTcpClient();
+Console.WriteLine("Server accepted: " + client.Client.RemoteEndPoint);
+string currentTime = "Tuesday, today!";
+
+byte[] bytes = Encoding.ASCII.GetBytes(currentTime);
+
+client.GetStream().Write(bytes);
+client.Close();
+
